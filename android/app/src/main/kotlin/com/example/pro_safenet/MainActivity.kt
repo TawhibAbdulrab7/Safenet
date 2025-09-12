5933ba74ea3949af4054a238a6a8aa3a4a9cd2d9
TawhibAbdulrab7 <tawheebalmahdi@gmail.com>
2025-09-12 04:37:58 +0300

    The First Commit in My Project Safenet

---
+++ android/app/src/main/kotlin/com/example/pro_safenet/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pro_safenet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import connectivity_plus
+import flutter_secure_storage_macos
+import network_info_plus
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
+}
