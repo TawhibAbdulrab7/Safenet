11d1cb0d5d467923d590a911609c537e51e4c0a7
TawhibAbdulrab7 <tawheebalmahdi@gmail.com>
2025-09-16 01:45:15 +0300

    Login Screen: add gradient and security icon

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blue, Colors.lightBlueAccent],
+          ),
+        ),
+        child: const Center(
+          child: Icon(Icons.security, size: 80, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
