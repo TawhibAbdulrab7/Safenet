0477afb3ad6900f1f30bd053c669653b8037ef3c
TawhibAbdulrab7 <tawheebalmahdi@gmail.com>
2025-09-16 02:58:05 +0300

    Login Screen: add login and signup buttons

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -15,42 +15,56 @@ class LoginScreen extends StatelessWidget {
           ),
         ),
 
-
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.security, size: 80, color: Colors.white),
-                const SizedBox(height: 20,),
-                const TextField(
-                  decoration: InputDecoration(
-                    labelText: "البريد الالكتروني",
-                    labelStyle: TextStyle(fontSize: 18,color:Colors.black ),
-                    prefixIcon: Icon(Icons.email),
-                    border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(15))
-                    ),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.security, size: 80, color: Colors.white),
+              const SizedBox(height: 20),
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: "البريد الالكتروني",
+                  labelStyle: TextStyle(fontSize: 18, color: Colors.black),
+                  prefixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
                   ),
                 ),
-                const SizedBox(height: 12,),
-                const TextField(
+              ),
+              const SizedBox(height: 12),
+              const TextField(
                 // cursorErrorColor: Colors.red,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: "كلمة المرور",
-                    labelStyle: TextStyle(fontSize: 18,color:Colors.black ),
-                    prefixIcon: Icon(Icons.lock),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(15))
-                    ),
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "كلمة المرور",
+                  labelStyle: TextStyle(fontSize: 18, color: Colors.black),
+                  prefixIcon: Icon(Icons.lock),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
                   ),
                 ),
-
-              ],
-            ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/dashboard');
+                },
+                child: const Text(
+                  "تسجيل الدخول",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  "أنشاء حساب جديد",
+                  style: TextStyle(fontSize: 20,color: Colors.white),
+                ),
+              ),
+            ],
           ),
         ),
-
+      ),
     );
   }
 }
