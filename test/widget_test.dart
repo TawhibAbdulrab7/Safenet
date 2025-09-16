4467b489a39488520b0548e02b7c0d1cdc6af63a
TawhibAbdulrab7 <tawheebalmahdi@gmail.com>
2025-09-16 03:33:33 +0300

    تعديل بسيط من أجل استدعاء البرنامج

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:pro_safenet/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SafeNetApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
